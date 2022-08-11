4092439f36a2b45139a885f6c323961da05c8733
bnurmos <bnurmos@gmail.com>
2022-08-11 09:16:25 -0600

    added page components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Todos</h1>
-      <Todo text='Learn React'/>
-      <Todo text='Master React'/>
-      <Todo text='Test React'/>
+    <div>
+     
     </div>
   );
 }
